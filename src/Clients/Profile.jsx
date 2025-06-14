7b0009be5ee61a37afe2dd8bcf9157931a50f973
Aaditya Sigdel <adityasigdel8@gmail.com>
2025-06-14 15:20:37 +0545

    profileFrontend

--- src/Clients/Profile.jsx
+++ src/Clients/Profile.jsx
@@ -4,6 +4,24 @@ export default function Profile() {
     return (
         <div>
             <Navigation />
+            <div className="flex flex-col items-center justify-center h-screen w-full" >
+                <h1 className='text-4xl font-bold text-[#0033A0] mb-6'> Your Profile</h1>
+                <img className='w-32 h-32 rounded-full bg-amber-50 border-1 mb-3'>
+                </img>
+                <ul className='flex flex-col gap-6 '>
+                    <li>Name</li>
+                    <li>Username</li>
+                    <li>Email</li>
+                    <li>Phone</li>
+                    <li>Address</li>
+
+                    <summary>
+                        <h1 className='text-2xl font-bold text-[#0033A0] mb-6'>About Me</h1>
+                        Hi my name is Aditya
+                    </summary>
+                </ul>
+
+            </div>
         </div>
     )
 }
